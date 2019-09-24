843a804465f3eb3398cbcf6e27335838f26e3151
le tong <le.tong@ly.com>
2019-09-24 13:44:19 +0800

    Initial Commit

---
+++ OneDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneDay
+//
+//  Created by le tong on 2019/9/24.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
